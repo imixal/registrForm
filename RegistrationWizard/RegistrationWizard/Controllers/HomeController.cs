7337a750ebb639662fbdfac3e758ab786d35fed2
mixal <i.mixal@yandex.ru>
2016-09-18 20:11:46 +0300

    Result

---
+++ RegistrationWizard/RegistrationWizard/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationWizard.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult RegisterMain()
+        {
+            return PartialView("_MainInfo");
+        }
+        public PartialViewResult RegisterType()
+        {
+            return PartialView("_RegisterType");
+        }
+        public PartialViewResult RegiserAddress()
+        {
+            return PartialView("_RegiserAddress");
+        }
+    }
+}
---
+++ RegistrationWizard/RegistrationWizard/Scripts/Script.js
@@ -0,0 +1,101 @@
+﻿$(document).ready(function () {
+    $("#Register").click(function () {
+        $("#preloder").show();
+        $.get("/Home/RegisterMain").done(function (data) {
+            $("#registration-wrapper").empty();
+            $("#preloder").hide();
+            $("#registration-wrapper").html(data);
+            $("#main-info").validate({
+                submitHandler: function(form) {
+                    $("#preloder").show();
+                    $.get("/Home/RegisterType").done(function (data) {
+                        $("#registration-wrapper").empty();
+                        $("#preloder").hide();
+                        $("#registration-wrapper").html(data);
+                        $("#NextAddress").click(function () {
+                            $.get("/Home/RegiserAddress").done(function (data) {
+                                $("registration-wrapper").empty();
+                                $("#registration-wrapper").html(data);
+                                $("#address-info").validate({
+                                    submitHandler: function (form) {
+                                        alert("Вы зарегистрированы!");
+                                        location.href = location.href;
+                                    },
+                                    rules: {
+                                        country: {
+                                            required:true,
+                                        },
+                                        phone: {
+                                            required: true,
+                                            digits: true,
+                                        },
+                                        address: {
+                                            required:true,
+                                        }
+                                    },
+                                    messages: {
+                                        country: {
+                                            required: "Это поле обязательно для заполнения",
+                                        },
+                                        phone: {
+                                            required: "Это поле обязательно для заполнения",
+                                            digits: "Только цифры",
+                                        },
+                                        address: {
+                                            required: "Это поле обязательно для заполнения",
+                                        }
+                                    }
+                                });
+                                $("#End").click(function () {
+                                    $("#End").valid();
+                                })
+                            });
+                        });
+                    });
+                },
+                rules: {
+                    login: {
+                        required: true,
+                        minlength: 4,
+                        maxlength: 16,
+                    },
+                    name: {
+                        required: true,
+                    },
+                    pass: {
+                        required: true,
+                        minlength: 6,
+                        maxlength: 16,
+                    },
+                    email: {
+                        required: true,
+                        email: true,
+                    }
+                },
+                messages:{
+
+                    login:{
+                         required: "Это поле обязательно для заполнения",
+                         minlength: "Логин должен быть минимум 4 символа",
+                         maxlength: "Максимальное число символо - 16",
+                    },
+                    name: {
+                        required: "Это поле обязательно для заполнения",
+                    },
+                    pass: {
+                        required: "Это поле обязательно для заполнения",
+                        minlength: "Пароль должен быть минимум 6 символа",
+                        maxlength: "Пароль должен быть максимум 16 символов",
+                    },
+                    email: {
+                        required: "Это поле обязательно для заполнения",
+                        email: "Некорректный Email",
+                    }
+                }
+            });
+            $("#Next").click(function () {
+                $("#Next").valid();
+            })
+        });
+    });
+});
